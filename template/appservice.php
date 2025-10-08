46b0d2ade721104ed6bd8d2525450b935721eeb7
keito noguchi <noguchi@hug-inc.co.jp>
2025-10-08 19:26:17 +0900

    新規追加

---
+++ template/appservice.php
@@ -0,0 +1,76 @@
+<section class="app_service">
+  <div class="sec_inner">
+    <div class="sec_title">
+      <h2><span>アプリ「ベルコメンバーズ」には<br>身近なサービスがいろいろ</span></h2>
+      <div class="app_service_title_image">
+        <img src="<?php echo esc_url(
+          get_template_directory_uri() . ( is_page('gymnastics_introduction')
+            ? '/img/app_service_title_image_pink.png'
+            : '/img/app_service_title_image.png'
+          )
+        ); ?>" alt="イメージ画像">
+      </div>
+    </div>
+    <ul class="app_service_list">
+      <li>
+        <div class="app_service_icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/app_service_icon01.png" alt="コンシェルジュサービス">
+        </div>
+        <h3>コンシェルジュサービス</h3>
+        <ul>
+          <li>おうちのお困りごと</li>
+          <li>お片付け・掃除</li>
+          <li>葬儀</li>
+          <li>供養・お墓</li>
+          <li>老後</li>
+          <li>相続</li>
+        </ul>
+        <p class="app_service_txt">
+          など沢山のサービスについてコンシェルジュが、お客様のニーズに合わせた専門業者をご紹介。
+        </p>
+      </li>
+      <li>
+        <div class="app_service_icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/app_service_icon02.png" alt="ベルコ体操">
+        </div>
+        <h3>ベルコ体操</h3>
+        <p class="app_service_txt">
+          ベルコのCMソングにわせて一緒に体を動かし、大人から子供までみんなで楽しめる体操です。<br>
+          他には、毎日を心地よくサポートする体操（就寝前、外出前など）に行う体操も紹介しています。
+        </p>
+      </li>
+      <li>
+        <div class="app_service_icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/app_service_icon03.png" alt="イベント検索">
+        </div>
+        <h3>イベント検索</h3>
+        <p class="app_service_txt">
+          お葬儀相談会、終活相談会、高齢者施設相談会、撮影体験会など沢山のイベントを全国から探せます。
+        </p>
+      </li>
+      <li>
+        <div class="app_service_icon">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/app_service_icon04.png" alt="施設検索">
+        </div>
+        <h3>施設検索</h3>
+        <p class="app_service_txt">
+          葬儀場、結婚式場、フォトスタジオ、老人ホーム紹介センター、ホテルなどの全国の施設を検索できます。
+        </p>
+      </li>
+    </ul>
+    <p class="app_service_lead">
+      そのほか、ラッキー占いや、<br>
+      ARフォトでベルちゃんルコちゃんと<br class="sp">写真を撮れるなど、<br>
+      楽しい機能も備えてます。
+    </p>
+    <div class="app_service_image">
+      <img src="<?php echo esc_url(
+        get_template_directory_uri() . ( is_page('gymnastics_introduction')
+          ? '/img/app_service_image_pink.png'
+          : '/img/app_service_image.png'
+        )
+      ); ?>" alt="イメージ画像">
+    </div>
+    </div>
+  </div>
+</section>
