39d4c05ce1855730ca150e75f7d425a5a0366df8
keito noguchi <noguchi@hug-inc.co.jp>
2025-11-20 21:44:45 +0900

    テキスト修正

--- template/appbox.php
+++ template/appbox.php
@@ -10,7 +10,7 @@
         <div class="appstore_logo">
           <img src="<?php echo get_template_directory_uri(); ?>/img/logo_bellcomembers.png" alt="ベルコメンバーズロゴ">
         </div>
-        <p class="app_detail_txt">【ベルコメンバーズアプリ】をダウンロードする</p>
+        <p class="app_detail_txt">認知機能チェックもできる、<br class="sp">暮らしの安心アプリ</p>
         <div class="btn_app_box">
           <div class="btn_appstore">
             <a href="https://apps.apple.com/jp/app/%E3%83%99%E3%83%AB%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%BA%E3%82%A2%E3%83%97%E3%83%AA/id6473576667?utm_source=bellco.co.jp&utm_medium=referral&utm_campaign=ninchisho_ios" target="_blank">
@@ -37,7 +37,7 @@
         <div class="app_image">
           <img src="<?php echo get_template_directory_uri(); ?>/img/appstore_image.png" alt="ベルコメンバーズ">
         </div>
-        <p class="app_detail_txt">【ベルコメンバーズアプリ】<br class="sp">をダウンロードする</p>
+        <p class="app_detail_txt">認知機能チェックもできる、<br class="sp">暮らしの安心アプリ</p>
         <div class="btn_app_box">
           <div class="btn_appstore">
             <a href="https://apps.apple.com/jp/app/%E3%83%99%E3%83%AB%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%BA%E3%82%A2%E3%83%97%E3%83%AA/id6473576667?utm_source=bellco.co.jp&utm_medium=referral&utm_campaign=ninchisho_ios" target="_blank">
