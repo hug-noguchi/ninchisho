bfe0a8101feedc80df59b44d808a166704187c64
keito noguchi <noguchi@hug-inc.co.jp>
2025-11-20 21:44:28 +0900

    アプリ共通ファイル記述へ

--- page-diagnostic_referral.php
+++ page-diagnostic_referral.php
@@ -76,62 +76,7 @@ get_header(); ?>
       </div>
     </div>
   </section>
-  <div class="app_box">
-    <div class="app_box_wrapper pc">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/diagnostic_referral/bg_app.png" class="bg_app_box">
-      <div class="sec_inner">
-        <div class="app_image">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/diagnostic_referral/appstore_image.png" alt="ベルコメンバーズ">
-        </div>
-        <div class="app_detail">
-          <p class="app_detail_title">\ 無料ダウンロード /</p>
-          <div class="appstore_logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/diagnostic_referral/logo_bellcomembers.png" alt="ベルコメンバーズロゴ">
-          </div>
-          <p class="app_detail_txt">【ベルコメンバーズアプリ】をダウンロードする</p>
-          <div class="btn_app_box">
-            <div class="btn_appstore">
-              <a href="https://apps.apple.com/jp/app/%E3%83%99%E3%83%AB%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%BA%E3%82%A2%E3%83%97%E3%83%AA/id6473576667?utm_source=bellco.co.jp&utm_medium=referral&utm_campaign=ninchisho_ios" target="_blank">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/btn_ios.svg" alt="appstore">
-              </a>
-            </div>
-            <div class="btn_googleplay">
-              <a href="https://play.google.com/store/search?q=%E3%83%99%E3%83%AB%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%BA&c=apps&pli=1&utm_source=bellco.co.jp&utm_medium=referral&utm_campaign=ninchisho_android" target="_blank">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/btn_android.svg" alt="googlepay">
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="app_box_wrapper sp">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/diagnostic_referral/bg_app_sp.png" class="bg_app_box">
-      <div class="sec_inner">
-        <div class="app_detail">
-          <p class="app_detail_title">\ 無料ダウンロード /</p>
-          <div class="appstore_logo">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/diagnostic_referral/logo_bellcomembers.png" alt="ベルコメンバーズロゴ">
-          </div>
-          <div class="app_image">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/diagnostic_referral/appstore_image.png" alt="ベルコメンバーズ">
-          </div>
-          <p class="app_detail_txt">【ベルコメンバーズアプリ】<br class="sp">をダウンロードする</p>
-          <div class="btn_app_box">
-            <div class="btn_appstore">
-              <a href="https://apps.apple.com/jp/app/%E3%83%99%E3%83%AB%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%BA%E3%82%A2%E3%83%97%E3%83%AA/id6473576667?utm_source=bellco.co.jp&utm_medium=referral&utm_campaign=ninchisho_ios" target="_blank">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/btn_ios.svg" alt="appstore">
-              </a>
-            </div>
-            <div class="btn_googleplay">
-              <a href="https://play.google.com/store/search?q=%E3%83%99%E3%83%AB%E3%82%B3%E3%83%A1%E3%83%B3%E3%83%90%E3%83%BC%E3%82%BA&c=apps&pli=1&utm_source=bellco.co.jp&utm_medium=referral&utm_campaign=ninchisho_android" target="_blank">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/btn_android.svg" alt="googlepay">
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <?php include get_template_directory() . '/template/appbox.php'; ?>
   <section class="merit">
     <div class="sec_inner">
       <h2>ベルコメンバーズでできる<br>「認知機能チェック」の3つのメリット</h2>
