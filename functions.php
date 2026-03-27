ad8512d505dc438627664448b58fdd193e463aec
keito noguchi <noguchi@hug-inc.co.jp>
2026-03-27 12:47:28 +0900

    記事内CTAテキスト修正

--- functions.php
+++ functions.php
@@ -393,7 +393,7 @@ add_action( 'init', 'change_post_tag_to_checkbox', 1 );
 function info_card_knowledge_func() {
   return '<div class="info-card info-card-knowledge">
             <a href="/ninchisho/basic_knowledge/">
-              <p class="info-card-lead"><span>認知症の基礎知識</span></p>
+              <p class="info-card-lead"><span>認知症を詳しく知りたい方に</span></p>
               <figure class="info-card-thumbnail">
                 <img src="https://www.bellco.co.jp/ninchisho/uBs8nV5c/wordpress/wp-content/themes/ninchisho/img/info-card-knowledge.png" alt="認知症の基礎知識" width="144" height="96">
               </figure>
